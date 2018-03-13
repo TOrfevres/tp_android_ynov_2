43be16a891accca147aac29261629b8df5ebee17
Théodore Orfèvres <majorkooll@gmail.com>
2018-03-13 17:21:39 +0100

    INIT

---
+++ app/src/main/java/com/ynov/android/to/tp_android_ynov_2/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.ynov.android.to.tp_android_ynov_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
